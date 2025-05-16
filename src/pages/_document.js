4d9a6818526bc9ab153f8cdd3c30be7ac242059e
Lisa-Ann B <lisabruney@pitt.edu>
2025-05-16 10:55:26 -0400

    Update meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      
+      <Head>
+        <link rel="icon" href="./logo.png" />
+        <title>UBKG Color Palettes</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
